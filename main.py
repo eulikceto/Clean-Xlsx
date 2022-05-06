8e8042ab652a69b4187910fd0da9abdcd384899e
olikceta <103790791+eulikceto@users.noreply.github.com>
2022-05-06 18:02:56 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from utils import *
+
+def main():
+	DataFrames = to_df()
+	df = pd.concat(DataFrames)
+	df_clean = remove_empty(df)
+	df_clean.to_excel('resultado.xlsx', index=False)
+	print('Finalizado!')
+if __name__ == '__main__': 
+	main()
