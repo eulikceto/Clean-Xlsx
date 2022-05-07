6a0c04d43964dc5a3c494084d5a3f1837f3c6100
olikceta <103790791+eulikceto@users.noreply.github.com>
2022-05-06 22:03:25 -0300

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,16 @@
+#imports
+import pandas as pd
+import os
+
+#Pega os arquivo .xlsx e faz uma lista com os DataFrame deles
+def to_df(path='./planilhas/') -> list:
+	files = [f'{path}{file}' for file in os.listdir(path) if file.endswith('.xlsx')]
+	print('Arquivos: ')
+	for i, file in enumerate(files):
+		print(f'    {i + 1} | {file}')
+	dfs = [pd.read_excel(excel) for excel in files]
+	return dfs
+
+#Remove todas as linhas de um dataframe cujo a coluna "Quantidade" é igual a 0
+def remove_empty(df) -> 'DataFrame':
+	 return df.loc[df["Quantidade"] != 0]
